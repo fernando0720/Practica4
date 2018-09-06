229109d999ac1a0cd4e13eac60a18ba71bb85f40
fernando0720 <fernandoa.s.p1553@gmail.com>
2018-09-05 23:40:15 -0400

    Resultado

---
+++ app/src/main/java/com/example/ferna/practica4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ferna.practica4;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
